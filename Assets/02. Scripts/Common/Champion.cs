8b67a3a8d323fa5263b0f3b6d2699c111a26837d
hoemins <121000970+hoemins@users.noreply.github.com>
2024-01-07 02:02:27 +0900

    메인씬 맵 디자인

--- Assets/02. Scripts/Common/Champion.cs
+++ Assets/02. Scripts/Common/Champion.cs
@@ -14,6 +14,8 @@ public class ChampionInfo
     [Header("챔피언 기본 스탯")]
     [SerializeField] private uint physicalAtk; // 공격력
     [SerializeField] private uint abilityAtk; // 주문력
+    [SerializeField] private uint phsicalDef; // 방어력
+    [SerializeField] private uint magicalResistance; // 마법 저항력
     [SerializeField] private uint abilityHaste; // 스킬 가속
     [SerializeField] private float moveSpeed; // 이동 속도
     [SerializeField] private float criticalChance; // 치명타 확률
@@ -22,6 +24,8 @@ public class ChampionInfo
     public uint Level { get { return level; } set { level = (level >= 18) ? 18 : value; } }
     public uint PhysicalAtk { get {  return physicalAtk; }}
     public uint AbilityAtk { get { return abilityAtk; }}
+    public uint PhsicalDef { get { return phsicalDef; }}
+    public uint MagicalResistance { get { return magicalResistance; }}  
     public float MoveSpeed { get {  return moveSpeed; }}
     public float CriticalChance { get { return criticalChance; }}
     public uint AbilityHaste { get { return abilityHaste; }}

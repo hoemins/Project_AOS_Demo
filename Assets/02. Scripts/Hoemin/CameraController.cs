d72451272453ac27b088f4bc1027540db399a659
hoemins <121000970+hoemins@users.noreply.github.com>
2024-01-09 02:48:07 +0900

    메인 씬 UI 추가, 에셋 추가

--- Assets/02. Scripts/Hoemin/CameraController.cs
+++ Assets/02. Scripts/Hoemin/CameraController.cs
@@ -11,16 +11,16 @@ public class CameraController : MonoBehaviour
     {
         // 문제점 : Screen.width = 상수(1920) 이므로 오른쪽으로 이동하는것은 되나 왼쪽으로 이동할 수 없음
         // 해결 : 너비 상수에 부동소수점을 곱하여 해결
-        if (Input.mousePosition.x >= Screen.width * 0.95f)
+        if (Input.mousePosition.x >= Screen.width * 0.98f)
             transform.position += Vector3.right * cameraMoveSpeed * Time.deltaTime;
 
-        if (Input.mousePosition.x <= Screen.width * 0.05f)
+        if (Input.mousePosition.x <= Screen.width * 0.02f)
             transform.position += Vector3.left * cameraMoveSpeed * Time.deltaTime;
 
-        if (Input.mousePosition.y >= Screen.height * 0.95f)
+        if (Input.mousePosition.y >= Screen.height * 0.98f)
             transform.position += Vector3.forward * cameraMoveSpeed * Time.deltaTime;
 
-        if (Input.mousePosition.y <= Screen.height * 0.05f)
+        if (Input.mousePosition.y <= Screen.height * 0.02f)
             transform.position += Vector3.back * cameraMoveSpeed * Time.deltaTime;
 
     }

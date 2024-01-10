1a12027add288e39c2ebf3175a069ac39894b396
hoemins <121000970+hoemins@users.noreply.github.com>
2024-01-11 02:30:44 +0900

    PlayerInput 클래스에 입력키 추가

--- Assets/02. Scripts/Hoemin/PlayerInput.cs
+++ Assets/02. Scripts/Hoemin/PlayerInput.cs
@@ -30,25 +30,37 @@ public class PlayerInput : MonoBehaviour
 
     private void Update()
     {
-        if(IsPressed(BUTTON.Q_BTN))
-        {
-            invoker.AddCommand(btnQ);
-        }
-        else if (IsPressed(BUTTON.W_BTN))
-        {
-            invoker.AddCommand(btnW);
-        }
+        SetInputButton();
     }
 
+    private void SetInputButton()
+    {
+        if (IsPressed(BUTTON.Q_BTN)) invoker.ExecuteCommand(btnQ);
+        else if (IsPressed(BUTTON.W_BTN)) invoker.ExecuteCommand(btnW);
+        else if (IsPressed(BUTTON.E_BTN)) invoker.ExecuteCommand(btnE);
+        else if (IsPressed(BUTTON.R_BTN)) invoker.ExecuteCommand(btnR);
+        else if (IsPressed(BUTTON.D_BTN)) invoker.ExecuteCommand(btnD);
+        else if (IsPressed(BUTTON.F_BTN)) invoker.ExecuteCommand(btnF);
+        else if (IsPressed(BUTTON.A_BTN)) invoker.ExecuteCommand(btnA);
+    }
 
     bool IsPressed(BUTTON button)
     {
         pressedBtn = button;
         if (Input.GetKey(KeyCode.Q))
             pressedBtn = BUTTON.Q_BTN;
-        if(Input.GetKey(KeyCode.W))
+        else if(Input.GetKey(KeyCode.W))
             pressedBtn = BUTTON.W_BTN;
-
+        else if (Input.GetKey(KeyCode.E))
+            pressedBtn = BUTTON.E_BTN;
+        else if (Input.GetKey(KeyCode.R))
+            pressedBtn = BUTTON.R_BTN;
+        else if (Input.GetKey(KeyCode.D))
+            pressedBtn = BUTTON.D_BTN;
+        else if (Input.GetKey(KeyCode.F))
+            pressedBtn = BUTTON.F_BTN;
+        else if (Input.GetKey(KeyCode.A))
+            pressedBtn = BUTTON.A_BTN;
 
         return (button == pressedBtn);
     }
---
+++ Assets/02. Scripts/Hoemin/Skill.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public abstract class Skill
+{
+    private Sprite skillImg;
+    private string name;
+    private uint level;
+    private uint requiredMP;
+    private uint requiredLevel;
+}
